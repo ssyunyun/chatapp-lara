8001f32c016e05acdedbe45edc6f576353a8b434
ssyunyun <1ss.yun025@gmail.com>
2019-05-20 21:32:22 +0900

    first commit

--- resources/views/chs/menu.blade.php
+++ resources/views/chs/menu.blade.php
@@ -1,7 +1,18 @@
 <!DOCTYPE html>
 <html>
   <head>
+    <!--<link rel="stylesheet" href="{{ asset('css/style.css')}}">--> 
+    <!--<script src ="js/jquery.min.js"></script>-->
+    <!--<script src="https://unpkg.com/vue/dist/vue.js"></script>-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/semantic-ui/2.2.10/semantic.min.css">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
+    
+    <meta name="viewport" content="width=device-width">
+    <meta charset="utf-8">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>Main Menu</title>
   </head>
 
 </html>
