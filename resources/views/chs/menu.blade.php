5f1a6b94c858d19022602af8c22239b9c0386f1a
ssyunyun <1ss.yun025@gmail.com>
2019-05-20 20:51:48 +0900

    first commit

--- resources/views/chs/menu.blade.php
+++ resources/views/chs/menu.blade.php
@@ -5,6 +5,7 @@
     <!--<script src ="js/jquery.min.js"></script>-->
     <script src="https://unpkg.com/vue/dist/vue.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/semantic-ui/2.2.10/semantic.min.css">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
     
     <meta name="viewport" content="width=device-width">
