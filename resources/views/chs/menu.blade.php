a8781196074d26b0a0af2ffb4224e815febc87cc
ssyunyun <1ss.yun025@gmail.com>
2019-05-20 21:44:16 +0900

    first commit

--- resources/views/chs/menu.blade.php
+++ resources/views/chs/menu.blade.php
@@ -16,26 +16,8 @@
   </head>
 
   <body>
-      <button class="ui green button" v-on:click="selectMypage">マイページ</button>
-      <button class="ui green button" v-on:click="selectSetting">設定</button>
-
-      <h1 class="ui dividing header">Main Menu</h1><br>
-      <form class="ui form">
-        <div class="field"><br>
-          <h2>グループ検索</h2>
-          <div class="field">
-            <input type="text" name="group-name" placeholder="検索...">
-          </div>
-        </div>
-      </form>
-
-      <h2>グループ一覧</h2>
-
-      <div class="ui list" v-for="(list, i) in groups">
-        <button class="ui large fluid button" v-on:click="selectGroup(i)">
-        @{{ list }}
-        </button>
-      </div>
+    <div class="ui text container" id = "menu">
+    </div>
 
   <!--<script src="{{ secure_asset(('js/menu.js')}}"></script>-->
   </body>
