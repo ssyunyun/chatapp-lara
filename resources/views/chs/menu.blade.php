deb9714f1dfbaa12cbabbbfa62de194f1f7cca53
ssyunyun <1ss.yun025@gmail.com>
2019-05-20 21:42:25 +0900

    first commit

--- resources/views/chs/menu.blade.php
+++ resources/views/chs/menu.blade.php
@@ -16,7 +16,8 @@
   </head>
 
   <body>
-    <div class="ui text container" id = "menu">
+      <button class="ui green button" v-on:click="selectMypage">マイページ</button>
+      <button class="ui green button" v-on:click="selectSetting">設定</button>
 
       <h1 class="ui dividing header">Main Menu</h1><br>
       <form class="ui form">
@@ -35,7 +36,6 @@
         @{{ list }}
         </button>
       </div>
-    </div>
 
   <!--<script src="{{ secure_asset(('js/menu.js')}}"></script>-->
   </body>
