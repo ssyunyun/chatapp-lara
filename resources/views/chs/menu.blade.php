bc17d75b37078cd708c022c4dc9d5deab18daa37
ssyunyun <1ss.yun025@gmail.com>
2019-05-20 21:37:49 +0900

    first commit

--- resources/views/chs/menu.blade.php
+++ resources/views/chs/menu.blade.php
@@ -16,6 +16,25 @@
   </head>
 
   <body>
+    <div class="ui text container" id = "menu">
+      <button class="ui green button" v-on:click="selectMypage">マイページ</button>
+      <button class="ui green button" v-on:click="selectSetting">設定</button>
+
+      <h1 class="ui dividing header">Main Menu</h1><br>
+      <form class="ui form">
+        <div class="field"><br>
+          <h2>グループ検索</h2>
+          <div class="field">
+            <input type="text" name="group-name" placeholder="検索...">
+          </div>
+        </div>
+      </form>
+
+      <h2>グループ一覧</h2>
+
+    </div>
+
+  <!--<script src="{{ secure_asset(('js/menu.js')}}"></script>-->
   </body>
         
 </html>
