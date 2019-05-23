bba88a2ea9936b2c309f34a769d465e410f9c931
ssyunyun <1ss.yun025@gmail.com>
2019-05-24 01:42:04 +0900

    test3

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,7 +5,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
 		<meta name="csrf-token" content="{{ csrf_token() }}">
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/semantic-ui/2.2.10/semantic.min.css">
-		<link rel="stylesheet" href="{{ asset('css/vue.css')}}">
+		
 		<title>Login</title>
 	</head>
 
